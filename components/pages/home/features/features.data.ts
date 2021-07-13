1a1f60e936a722d9d41578d5bcd9b06e4a05de0d
AutoStake-Token <87110745+AutoStake-Token@users.noreply.github.com>
2021-07-13 05:44:09 -0400

    Update features.data.ts

--- components/pages/home/features/features.data.ts
+++ components/pages/home/features/features.data.ts
@@ -1,22 +1,20 @@
 export const featuresdata = [
     {
-        heading: "Strong Security",
-        details: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor, quis suscipit fugiat soluta tenetur sunt eveniet minima laudantium! Quia, odio.",
-        icon: "/assets/images/features/f1.png"
+        heading: "Earn Passive BNB",
+        details: "Each week holders will be airdropped rewards in BNB. Make an income just by holding."
     },
     {
-        heading: "Earn Quickly",
-        details: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor, quis suscipit fugiat soluta tenetur sunt eveniet minima laudantium! Quia, odio.",
-        icon: "/assets/images/features/f2.png"
+        heading: "Asset-Backed",
+        details: "AutoStake invests in PancakeSwap syrup pools and passes the rewards on to you, with an added bonus!"
     },
     {
-        heading: "Strong Community",
-        details: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor, quis suscipit fugiat soluta tenetur sunt eveniet minima laudantium! Quia, odio.",
+        heading: "Volume Independent",
+        details: "High volume or no volume, your rewards from the staking pool can only increase. No HODL-style volume death.",
         icon: "/assets/images/features/f3.png"
     },
     {
-        heading: "Secure Earning",
-        details: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor, quis suscipit fugiat soluta tenetur sunt eveniet minima laudantium! Quia, odio.",
+        heading: "Fully Doxxed",
+        details: "Our team is fully doxxed, talented, and here to stay. A project is only as good as the team behind it.",
         icon: "/assets/images/features/f4.png"
     },
 ]
