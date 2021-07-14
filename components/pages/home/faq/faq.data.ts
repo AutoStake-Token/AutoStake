19c0a356cd0e0c6bb0b839b235cac5f051bdf031
AutoStake-Token <87110745+AutoStake-Token@users.noreply.github.com>
2021-07-14 12:44:05 +0800

    Update faq.data.ts

--- components/pages/home/faq/faq.data.ts
+++ components/pages/home/faq/faq.data.ts
@@ -5,19 +5,15 @@ export const faqdata = [
     },
     {
         question: "How does this work?",
-        answer: "Instead of picking tokens or BNBs from transactions like Safemoon or HODL do, AutoStake charges a fee which it instead places in a Mining Station, in most cases Cake Syrup Pool, or BNB / USD staking. And the profit those Mining Station generate and sent out to the holders. This means that AutoStake also protects itself from the risk of a death spiral, which HODL and similar Tokens end up in, ie reduced trade, reduced reward and even more people selling.Since the AutoStake reward will always increase, the holders were not harmed by someone selling, but only create more incentives to buy more and take more of the reward pool. "
+        answer: "Instead of picking tokens or BNBs from transactions like Safemoon or HODL do, AutoStake charges a fee which it instead places in to a AutoStake Depot, in most cases Cake Syrup Pool, or BNB / USD staking. And the profit from the AutoStake Depot generated will then be sent out to the holders. This means that AutoStake also protects itself from the risk of a death spiral, which HODL and similar Tokens end up in, ie reduced trade, reduced reward and even more people selling.Since the AutoStake reward will always increase, the holders were not harmed by someone selling, but only create more incentives to buy more and take more of the reward pool. "
     },
     {
         question: "How do you get your reward?",
-        answer: "You do not need claim your rewards, your rewards are sent to your wallet where you keep your AutoStake Tokens. So there is no risk that you will miss your dividend. The distribution takes place in different instances depending on the Mining Station."
+        answer: "You do not need claim your rewards, your rewards are sent to your wallet where you keep your AutoStake Tokens. So there is no risk that you will miss your dividend. The distribution takes place in different instances depending on the AutoStake Depot."
     },
      {
         question: "How do you buy AutoStake?",
         answer: "You can buy AutoStake on PancakeSwap using our Contract Address: 
     },
-     {
-        question: "How do you get your reward?",
-        answer: "You do not need claim your rewards, your rewards are sent to your wallet where you keep your AutoStake Tokens. So there is no risk that you will miss your dividend. The distribution takes place in different instances depending on the Mining Station."
-    },
-
+        
 ]
