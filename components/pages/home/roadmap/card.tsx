971bc010dfe0be8fa782824130355a5cd5ff7d10
dex <40684326+dexmorg9@users.noreply.github.com>
2021-09-27 00:23:54 +0530

    ReBased

--- components/pages/home/roadmap/card.tsx
+++ components/pages/home/roadmap/card.tsx
@@ -1,22 +1,22 @@
-import tw from "twin.macro"
+import tw from "twin.macro";
 function Card({ data, index }) {
-    const isEven = index % 2 == 0 ? true : false;
+  const isEven = index % 2 == 0 ? true : false;
 
-    return (
-        <div tw="flex flex-1 w-full ">
-            <div tw="flex-1">
-                <div tw="flex flex-col px-4 border-l border-color[var(--red)] text-3xl color[var(--red)]">
-                    <span>{data.year}</span>
-                    <span tw="ml-12">{data.month} {data.year}</span>
-                </div>
-            </div>
-            <div tw="flex-1 flex flex-col color[var(--redDark)] text-2xl md:text-3xl space-y-8">
-                {data.events.map((item) =>
-                    <span>{item}</span>
-                )}
-            </div>
+  return (
+    <div tw="flex flex-1 w-full ">
+      <div tw="flex-1">
+        <div tw="flex flex-col px-4 border-l border-color[var(--red)] text-3xl color[var(--red)]">
+          <span>{data.year}</span>
+          <span tw="ml-12">{data.month}</span>
         </div>
-    )
+      </div>
+      <div tw="flex-1 flex flex-col color[var(--redDark)] text-2xl md:text-3xl space-y-8">
+        {data.events.map((item) => (
+          <span>{item}</span>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Card
+export default Card;
